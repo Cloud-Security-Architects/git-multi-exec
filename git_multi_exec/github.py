bfd6f5b7dcf90d5c2a2cabefdbe1f4e9e0681bea
Jonas Rosenboom <jrosenboom@checkpoint.com>
2024-07-24 08:58:40 -0400

    adds --org argument for GitHub scans

--- git_multi_exec/github.py
+++ git_multi_exec/github.py
@@ -1,6 +1,6 @@
 import logging
 
-from github import Github
+from github import Github, GithubException
 
 from .util import RemoteCallback, clone_and_run
 
@@ -21,8 +21,24 @@ class Runner:
         orgs = self.gh.get_user().get_orgs()
         for org in orgs:
             log.debug("Scanning org %s", org.login)
+            try:
+                repos = org.get_repos("member")
+                for repo in repos:
+                    clone_and_run(
+                        repo.clone_url, command=self.command, callback=self.callback
+                    )
+            except GithubException as e:
+                log.warn("Failed to scan org %s: %s", org.login, e)
+
+
+    def scan_org(self, org):
+        org = self.gh.get_organization(org)
+        log.debug("Scanning org %s", org.login)
+        try:
             repos = org.get_repos("member")
             for repo in repos:
                 clone_and_run(
                     repo.clone_url, command=self.command, callback=self.callback
                 )
+        except GithubException as e:
+            log.warn("Failed to scan org %s: %s", org.login, e)
